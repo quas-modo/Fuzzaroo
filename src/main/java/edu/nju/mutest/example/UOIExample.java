aca03223e71e9d74b6de03783fd1920d5d2ed597
Ruilin Geng <1585363499@qq.com>
2023-12-04 21:25:25 +0800

    add uoi testsuite

--- src/main/java/edu/nju/mutest/example/UOIExample.java
+++ src/main/java/edu/nju/mutest/example/UOIExample.java
@@ -3,15 +3,20 @@ package edu.nju.mutest.example;
 public class UOIExample {
     private UOIExample() {}
 
-    public static int pabs(int a) {
+    public static int bitWise(int a) {
         int b = 3;
         a = ~a + b;  // Unary operator applied: BITWISE_COMPLEMENT
         return a;
     }
 
-    public static int nabs(int a) {
+    public static int minus(int a) {
         int b = 9;
         a = -a + b;  // Unary operator applied: MINUS
         return a;
+    }
+
+    public static boolean equal(int a) {
+        int b = 10;
+        return (a == b);  // Binary operator applied: EQUAL
     }
 }
